be8d6aadb20956f3a09400013325a91d0b25b5a2
kingdilip <36642864+kingdilip@users.noreply.github.com>
2018-02-20 23:23:49 +0530

    Initial commit

---
+++ Barcode/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace Barcode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string brcode = textBox1.Text;
+            Bitmap bitmap = new Bitmap(brcode.Length * 40, 150);
+            using(Graphics graphics = Graphics.FromImage(bitmap))
+            {            
+            Font ofont = new System.Drawing.Font("IDAutomationHC39M", 20);
+            PointF point = new PointF(2f, 2f);
+            SolidBrush black = new SolidBrush(Color.Black);
+            SolidBrush white = new SolidBrush(Color.White);
+            graphics.FillRectangle(white, 0, 0, bitmap.Width, bitmap.Height);
+            graphics.DrawString(brcode , ofont, black, point);
+
+            }
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                pictureBox1.Image = bitmap;
+                pictureBox1.Height = bitmap.Height;
+                pictureBox1.Width = bitmap.Width;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+        }
+    }
+}
---
+++ Barcode/Form2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barcode
+{
+    public partial class Form2 : Form
+    {
+         SerialPort _serialPort;  
+  
+     private delegate void SetTextDeleg(string text);
+     public Form2()
+     {
+         InitializeComponent();
+
+     } 
+  
+     private void Form1_Load(object sender, EventArgs e)  
+     {  
+           _serialPort = new SerialPort("COM1", 19200, Parity.None, 8, StopBits.One);  
+           _serialPort.Handshake = Handshake.None;  
+           _serialPort.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);  
+           _serialPort.ReadTimeout = 500;  
+           _serialPort.WriteTimeout = 500;  
+           _serialPort.Open();  
+     }  
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try  
+           {  
+                if(!_serialPort.IsOpen)  
+                     _serialPort.Open();  
+  
+                _serialPort.Write("SI\r\n");  
+           }  
+           catch (Exception ex)  
+           {  
+                MessageBox.Show("Error opening/writing to serial port :: " + ex.Message, "Error!");  
+           }  
+        }
+         void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)  
+     {  
+          Thread.Sleep(500);  
+          string data = _serialPort.ReadLine();  
+          this.BeginInvoke(new SetTextDeleg(si_DataReceived), new object[] { data });  
+     }  
+  
+     private void si_DataReceived(string data)  
+     {  
+           textBox1.Text = data.Trim();  
+     }  
+    }
+}
